d28eda5d220ddb4db2c6a0c6ff4b095d4abd1570
BaronAldrichReyes <151493978+BaronAldrichReyes@users.noreply.github.com>
2026-07-06 18:08:21 +0800

    Update views.py

--- invitations/views.py
+++ invitations/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html') # Or whatever your template file is named!
+    return render(request, 'rsvp.html') # Or whatever your template file is named!
