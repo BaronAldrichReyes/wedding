47551a30504e25d335071c2c4cf39a5334fb9da4
Baron Aldrich <bArich252@gmail.com>
2026-08-02 16:47:11 +0800

    Add guest database migrations

--- wedding_project/settings.py
+++ wedding_project/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'wedding_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wedding_db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlite3'),
         'USER': 'postgres',
         'PASSWORD': 'baron252',
         'HOST': 'localhost',
