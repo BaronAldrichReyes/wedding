8c081a307cb722375672efb5a268cf9f5b18f46c
Baron Aldrich <bArich252@gmail.com>
2026-08-08 12:18:44 +0800

    Fix find_seat view to support full name searching

--- guests/views.py
+++ guests/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages  # <-- Imports the pop-up tool
 from .forms import RSVPForm
 from django.db.models import Q
 from .models import Guest
+from django.db.models import Value
+from django.db.models.functions import Concat
 
 def rsvp_page(request):
     if request.method == 'POST':
@@ -23,10 +25,12 @@ def find_seat(request):
     query = request.GET.get('name')
     
     if query:
-        # Search if the query matches either the first name OR the last name
-        guest = Guest.objects.filter(
-            Q(first_name__icontains=query) | Q(last_name__icontains=query)
-        ).first()
+        clean_query = query.strip()
+
+        guest = Guest.objects.annotate(
+            full_name=Concat('first_name', Value(' '), 'last_name')
+        ).filter(full_name__icontains=clean_query).first()
+        
         return render(request, 'guests/seat_result.html', {'guest': guest, 'query': query})
         
     return render(request, 'guests/seat_result.html')
